4a05968c1cf0fe0516bf01a745ec3d231be2e387
Michael Morrison <mmorrison@wootalyzer.com>
2013-02-06 23:26:22 -0600

    clean before build

--- nkbuild.bat
+++ nkbuild.bat
@@ -1,2 +1,3 @@
 qmake CONFIG-=sse2 CONFIG+=no-asio CONFIG+=no-g15 CONFIG+=no-bonjour CONFIG+=no-elevation CONFIG+=no-server CONFIG+=packaged CONFIG+=no-plugins -recursive
+nmake clean
 nmake release
