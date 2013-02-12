b5064ec150b988e7b83242461d5aa398c41bc086
Michael Morrison <mmorrison@wootalyzer.com>
2013-02-11 18:04:04 -0600

    make both sse and sse2?

--- nkbuild.bat
+++ nkbuild.bat
@@ -1,10 +1,19 @@
+rd /s /q release
+mkdir release
+
+qmake ^
+	CONFIG-=sse2 ^
+	CONFIG+=no-bundled-speex ^
+	CONFIG+=no-plugins CONFIG+=no-asio CONFIG+=no-g15 ^
+	CONFIG+=no-bonjour CONFIG+=no-server ^
+	CONFIG+=packaged -recursive
+nmake clean
+nmake release
+
 qmake ^
 	CONFIG+=sse2 ^
 	CONFIG+=no-bundled-speex ^
 	CONFIG+=no-plugins CONFIG+=no-asio CONFIG+=no-g15 ^
 	CONFIG+=no-bonjour CONFIG+=no-server ^
 	CONFIG+=packaged -recursive
-rd /s /q release
-mkdir release
-nmake clean
 nmake release
