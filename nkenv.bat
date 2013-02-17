7aa69e2079634ca8582d9c7cda3247ab96034f63
Michael Morrison <mmorrison@wootalyzer.com>
2013-02-17 14:42:34 -0600

    update to new toolchain

--- nkenv.bat
+++ nkenv.bat
@@ -2,34 +2,29 @@
 
 SET MumbleVersion=1.0.8
 
-SET DIR=d:\mumble
+SET TOOLCHAIN=d:\toolchain
+
 SET VSVER=10.0
 SET LIB=
-SET QT_DIR=%DIR%\qtmumble
-SET JOM_DIR=%DIR%\jom
-SET MUMBLE_DIR=%DIR%\mumble
-SET VLD_DIR=%DIR%\vld
-SET MYSQL_DIR=%DIR%\mysql
-SET NASM_DIR=%DIR%\nasm
-SET ICE_DIR=%DIR%\ice
-SET PROTOBUF_DIR=%DIR%\protobuf
-SET LIBSNDFILE_DIR=%DIR%\libsndfile
-SET OPENSSL_DIR=%DIR%\openssl
-SET BOOST_DIR=%DIR%\boost
-SET TOOLCHAIN=d:\chrome\win_toolchain
-SET GITBIN_DIR=d:\chrome\depot_tools
-SET PERLBIN_DIR=%DIR%\perl\bin
-SET ZLIB_DIR=%DIR%\zlib
+SET QT_DIR=%TOOLCHAIN%\qtmumble
+SET JOM_DIR=%TOOLCHAIN%\jom
+SET VLD_DIR=%TOOLCHAIN%\vld
+SET MYSQL_DIR=%TOOLCHAIN%\mysql
+SET NASM_DIR=%TOOLCHAIN%\nasm
+SET ICE_DIR=%TOOLCHAIN%\ice
+SET PROTOBUF_DIR=%TOOLCHAIN%\protobuf
+SET LIBSNDFILE_DIR=%TOOLCHAIN%\libsndfile
+SET OPENSSL_DIR=%TOOLCHAIN%\openssl
+SET BOOST_DIR=%TOOLCHAIN%\boost
+SET GITBIN_DIR=%TOOLCHAIN%\bin
+SET PERLBIN_DIR=%TOOLCHAIN%\perl\bin
+SET ZLIB_DIR=%TOOLCHAIN%\zlib
 SET DEBUGTOOLS_DIR=%TOOLCHAIN%\WDK\bin
 SET CODESIGN_CERT=d:\jenkins\secure\code.p12
 
-SET OLDDIR=%CD%
-CALL "%TOOLCHAIN%\env.bat"
-::Set Lib=%DXSDK_DIR%\Lib\x86;%Lib%
-::Set Include=%DXSDK_DIR%\Include;%Include%
-SET DXSDK_DIR=%DXSDK_DIR%\
-CALL "%TOOLCHAIN%\VC\vcvarsall.bat" x86
+SET DXSDK_DIR=%TOOLCHAIN%\dxsdk\
+SET WindowsSDKDir=%TOOLCHAIN%\win8sdk
+CALL "%TOOLCHAIN%\mcvs2010\VC\vcvarsall.bat" x86
 SET PATH=%TOOLCHAIN%\win8sdk\bin\x86;%GITBIN_DIR%;%PERLBIN_DIR%;%QT_DIR%\bin;%JOM_DIR%;%OPENSSL_DIR%\bin;%LIBSNDFILE_DIR%\bin;%MYSQL_DIR%\lib;%ICE_DIR%\bin\vc100;%PROTOBUF_DIR%\vsprojects\Release;%NASM_DIR%;%VLD_DIR%\bin;%PATH%
 
-chdir /d %OLDDIR%
 TITLE Mumble Development Environment
