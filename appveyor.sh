1ba9e474a298236a89e4e6a2b2e2fe64aeabd885
Michael Morrison <mmorrison@pon4.net>
2015-02-11 04:08:02 -0600

    test

---
+++ appveyor.sh
@@ -0,0 +1,60 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+MumbleVersion=1.1.$APPVEYOR_BUILD_NUMBER
+
+TOOLCHAIN=$DIR/toolchain
+
+VSVER=10.0
+LIB=
+# QT_DIR=C:/Qt/Qt5.2.1/5.2.1/msvc2010
+JOM_DIR=$TOOLCHAIN/jom
+VLD_DIR=$TOOLCHAIN/vld
+MYSQL_DIR=$TOOLCHAIN/mysql
+NASM_DIR=$TOOLCHAIN/nasm
+ICE_DIR=$TOOLCHAIN/ice
+PROTOBUF_DIR=$TOOLCHAIN/protobuf
+LIBSNDFILE_DIR=$TOOLCHAIN/libsndfile
+OPENSSL_DIR=$TOOLCHAIN/openssl
+BOOST_DIR=$TOOLCHAIN/boost
+GITBIN_DIR=$TOOLCHAIN/bin
+PERLBIN_DIR=$TOOLCHAIN/perl/bin
+ZLIB_DIR=$TOOLCHAIN/zlib
+DEBUGTOOLS_DIR=$TOOLCHAIN/WDK/bin
+CODESIGN_CERT=d:/jenkins/secure/code.p12
+NSIS_DIR=$TOOLCHAIN/nsis
+
+# QMAKESPEC=%QT_DIR%/mkspecs/win32-msvc2010
+
+# DXSDK_DIR=$TOOLCHAIN/dxsdk/
+# WindowsSDKDir=$TOOLCHAIN/win8sdk
+# CALL "%WindowsSDKDir%/bin/SetEnv.cmd"
+# PATH=$TOOLCHAIN/win8sdk/bin/x86;C:/Windows/Microsoft.NET/Framework/v4.0.30319;%GITBIN_DIR%;%PERLBIN_DIR%;%QT_DIR%/bin;%JOM_DIR%;%OPENSSL_DIR%/bin;%LIBSNDFILE_DIR%/bin;%MYSQL_DIR%/lib;%ICE_DIR%/bin/vc100;%PROTOBUF_DIR%/vsprojects/Release;%NASM_DIR%;%VLD_DIR%/bin;%PATH%
+
+rm -Rf release
+mkdir release
+
+qmake \
+	CONFIG-=sse2 \
+	CONFIG+=no-plugins CONFIG+=no-asio CONFIG+=no-g15 \
+	CONFIG+=no-bonjour CONFIG+=no-server \
+	CONFIG+=packaged -recursive
+
+jom clean
+jom -j4 release
+
+signtool sign \
+	/f %CODESIGN_CERT% \
+	/t "http://timestamp.verisign.com/scripts/timstamp.dll" \
+	release/*.exe release/*.dll release/plugins/*.dll
+
+rm -f mumble9000_installer/Mumble9000_install.exe
+rm -f Mumble9000_install.exe
+
+%NSIS_DIR%/Bin/makensis mumble9000_installer/mumble9000.nsi
+
+signtool sign \
+/f %CODESIGN_CERT% \
+/t "http://timestamp.verisign.com/scripts/timstamp.dll" \
+mumble9000_installer/Mumble9000_install.exe
+
+mv mumble9000_installer/Mumble9000_install.exe Mumble9000.$MumbleVersion.exe
