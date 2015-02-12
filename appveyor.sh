a44a625b01dc296c3e8b8ad5baa26b9b61baa555
Michael Morrison <mmorrison@pon4.net>
2015-02-11 22:10:50 -0600

    bump

--- appveyor.sh
+++ appveyor.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -e; set -u
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-MumbleVersion=1.1.$APPVEYOR_BUILD_NUMBER
+MumbleVersion="1.1.$APPVEYOR_BUILD_NUMBER"
 
-TOOLCHAIN=$DIR/toolchain
+TOOLCHAIN="$DIR/toolchain"
 
-VSVER=10.0
-LIB=
-QT_DIR=c:/Qt/5.4/msvc2013_opengl
-JOM_DIR=$TOOLCHAIN/jom
-VLD_DIR=$TOOLCHAIN/vld
-MYSQL_DIR=$TOOLCHAIN/mysql
-NASM_DIR=$TOOLCHAIN/nasm
-ICE_DIR=$TOOLCHAIN/ice
-PROTOBUF_DIR=$TOOLCHAIN/protobuf
-LIBSNDFILE_DIR=$TOOLCHAIN/libsndfile
-OPENSSL_DIR=$TOOLCHAIN/openssl
-BOOST_DIR=$TOOLCHAIN/boost
-ZLIB_DIR=$TOOLCHAIN/zlib
-DEBUGTOOLS_DIR=$TOOLCHAIN/WDK/bin
-CODESIGN_CERT=d:/jenkins/secure/code.p12
-NSIS_DIR=$TOOLCHAIN/nsis
+VSVER="10.0"
+LIB=""
+QT_DIR="c:/Qt/5.4/msvc2013_opengl"
+JOM_DIR="$TOOLCHAIN/jom"
+VLD_DIR="$TOOLCHAIN/vld"
+MYSQL_DIR="$TOOLCHAIN/mysql"
+NASM_DIR="$TOOLCHAIN/nasm"
+ICE_DIR="$TOOLCHAIN/ice"
+PROTOBUF_DIR="$TOOLCHAIN/protobuf"
+LIBSNDFILE_DIR="$TOOLCHAIN/libsndfile"
+OPENSSL_DIR="$TOOLCHAIN/openssl"
+BOOST_DIR="$TOOLCHAIN/boost"
+ZLIB_DIR="$TOOLCHAIN/zlib"
+DEBUGTOOLS_DIR="$TOOLCHAIN/WDK/bin"
+CODESIGN_CERT="d:/jenkins/secure/code.p12"
+NSIS_DIR="$TOOLCHAIN/nsis"
 
 # QMAKESPEC=%QT_DIR%/mkspecs/win32-msvc2010
 
@@ -30,7 +30,9 @@ NSIS_DIR=$TOOLCHAIN/nsis
 # CALL "%WindowsSDKDir%/bin/SetEnv.cmd"
 # PATH=$TOOLCHAIN/win8sdk/bin/x86;C:/Windows/Microsoft.NET/Framework/v4.0.30319;%QT_DIR%/bin;%JOM_DIR%;%OPENSSL_DIR%/bin;%LIBSNDFILE_DIR%/bin;%MYSQL_DIR%/lib;%ICE_DIR%/bin/vc100;%PROTOBUF_DIR%/vsprojects/Release;%NASM_DIR%;%VLD_DIR%/bin;%PATH%
 
-PATH=$QT_DIR/bin
+PATH="$QT_DIR/bin:$PATH"
+echo $PATH
+ls c:/cygwin/bin
 
 rm -Rf release
 mkdir release
@@ -59,4 +61,4 @@ signtool sign \
 /t "http://timestamp.verisign.com/scripts/timstamp.dll" \
 mumble9000_installer/Mumble9000_install.exe
 
-mv mumble9000_installer/Mumble9000_install.exe Mumble9000.$MumbleVersion.exe
+mv mumble9000_installer/Mumble9000_install.exe "Mumble9000.$MumbleVersion.exe"
